Solved bug in jacobian Added some tests for the integrators (It still needs a proper framework!) Added some plots in the simple run notebook

model/definition.py
@@ -5,7 +5,7 @@ from numba import njit
 from inner_products.analytic import AtmosphericInnerProducts
 from tensors.atensor import AtmosphericTensor
 from tensors.cootensor import from_csr_mat_list
-from functions.sparse import sparse_mul3
+from functions.sparse import sparse_mul3, sparse_mul_jac
 
 
 def create_tendencies(params):
@@ -13,7 +13,6 @@ def create_tendencies(params):
     aip= AtmosphericInnerProducts(params)
     atensor = AtmosphericTensor(aip)
     coo_atensor = from_csr_mat_list(atensor.tensor)
-    #jcoo_atensor = from_csr_mat_list(atensor_x.jacobian_tensor)
 
     coo = coo_atensor.coo
     val = coo_atensor.value
@@ -40,9 +39,8 @@ def create_linearized_tendencies(params):
     @njit
     def Df(t, x):
         xx = np.concatenate((np.full((1,), 1.), x))
-        xr = sparse_mul3(jcoo, jval, xx)
-
-        return xr[1:]
+        mul_jac = sparse_mul_jac(jcoo, jval, xx)
+        return mul_jac[1:, 1:]
 
     return Df
 