27acae0f7f2a176717cd7f74adaea42427a2f1d1
Jonathan Demaeyer <jodemaey@meteo.be>
2020-12-20 10:46:43 +0100

    Changed the version number to v0.2.1

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Jonathan Demaeyer and Lesley De Cruz'
 author = 'Jonathan Demaeyer and Lesley De Cruz'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.2.0'
+release = 'v0.2.1'
 version = release
 
 
